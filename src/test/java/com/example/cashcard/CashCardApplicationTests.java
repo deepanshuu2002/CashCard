0b0b731101fac3839adcce50c4bcdce3ff51211a
Deepanshu <Deepanshu@DAC-0181.local>
2025-06-17 19:47:23 +0530

    start

---
+++ src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
